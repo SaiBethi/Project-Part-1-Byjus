caa7f8ec461a676e03549ad11146d115fed8af24
SaiBethi <86983933+SaiBethi@users.noreply.github.com>
2022-02-12 12:45:18 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function preload()
+{
+
+Boss = loadImage("BossImg.jpg");
+Background = loadImage("Fighting.jpg");
+Marcus = loadImage("Marcus.png");
+Mark = loadImage("Mark.png");
+Mason = loadImage("Mason.png");
+Medkit = loadImage("Medkit.png");
+Michael = loadImage("Michael.png");
+NonPlayer= loadImage("NPC.png");
+Shield = loadImage("Shield.png");
+
+}
+
+function setup() 
+{
+
+createCanvas(WindowWidth,WindowHeight);
+
+
+}
